347edf7727821913228a805474c2dde8f6366b12
ahadsts9901 <ahadsts990@gmail.com>
2024-10-10 14:18:13 +0500

    splash-screen

--- src/pages/splash-screen/SplashScreen.tsx
+++ src/pages/splash-screen/SplashScreen.tsx
@@ -1,8 +1,35 @@
 import "./SplashScreen.css"
+import logo from "/logo.webp"
+import * as React from 'react'
+import LinearProgress from '@mui/material/LinearProgress';
 
 const SplashScreen = () => {
+
+    const [progress, setProgress] = React.useState(0);
+
+    React.useEffect(() => {
+        const timer = setInterval(() => {
+            setProgress((oldProgress) => {
+                if (oldProgress === 100) {
+                    return 100;
+                }
+                const diff = Math.random() * 10;
+                return Math.min(oldProgress + diff, 100);
+            });
+        }, 150);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
     return (
-        <div>SplashScreen</div>
+        <>
+            <div className="splashCont">
+                <img src={logo} alt="logo" />
+                <LinearProgress variant="determinate" value={progress} sx={{ width: "300px" }} />
+            </div>
+        </>
     )
 }
 
